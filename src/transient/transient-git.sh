654ae8d18843e2b836381ecc7174b3df0a777487
murer <murer@ciandt.com>
2022-10-03 12:33:19 -0300

    x

--- src/transient/transient-git.sh
+++ src/transient/transient-git.sh
@@ -11,15 +11,37 @@ function cmd_download() {
 function cmd_convert_commits() {
     local metriclite_gitname="${1?'metriclite_gitname'}"
     cd "target/git/$metriclite_gitname"
-    # git log --all --pretty=format:'commit: %H %nsubject: %s %nauthor: %aN %nemail: %aE %ndate: %at%nparents: %P%nRefs: %D'
     local gitcols='%H,%at,%P,%D,%aN,%aE,%s'
-    echo "$gitcols" | tr -d '%' > "../../../target/git/$metriclite_gitname.csv"
+    echo "$gitcols" | tr -d '%' > "../../../target/git/commits-$metriclite_gitname.csv"
     local gitsepcols="$(echo -n "__METRICLITEINIT__${gitcols}__METRICLITEINIT__" | sed 's/,/__METRICLITESEP__/g')"
     git log --all "--pretty=format:$gitsepcols" | \
         sed 's/"/""/g' | \
         sed 's/__METRICLITESEP__/","/g' | \
-        sed 's/__METRICLITEINIT__/"/g' >> "../../../target/git/$metriclite_gitname.csv"
-    gzip -f "../../../target/git/$metriclite_gitname.csv"
+        sed 's/__METRICLITEINIT__/"/g' >> "../../../target/git/commits-$metriclite_gitname.csv"
+    gzip -f "../../../target/git/commits-$metriclite_gitname.csv"
+    cd -
+}
+
+function cmd_convert_files() {
+    local metriclite_gitname="${1?'metriclite_gitname'}"
+    cd "target/git/$metriclite_gitname"
+    # git log --name-status --all '--pretty=format:"%n"%H","' | tail -c +3 | hea
+    echo "n,k,v" > "../../../target/git/files-$metriclite_gitname.csv"
+    # local commithash=""
+    # set +x
+    # git log --name-status --all '--pretty=format:ID%x09%H' | grep -v '^$' | while read k; do
+    #     if [[ "x$(echo "$k" | cut -f1)" == "xID" ]]; then
+    #         commithash="$(echo "$k" | cut -f2)"
+    #     else
+    #         echo -e "$commithash\t$k" | sed 's/\t/,/g' >> "../../../target/git/files-$metriclite_gitname.csv"
+    #     fi
+    # done
+    # set -x
+    git log --name-status --all '--pretty=format:ID%x09%H' | \
+        grep -n '.\+' | \
+        sed 's/:/,/1' | \
+        sed 's/\t/,/1' >> "../../../target/git/files-$metriclite_gitname.csv"
+    gzip -f "../../../target/git/files-$metriclite_gitname.csv"
     cd -
 }
 
@@ -31,7 +53,15 @@ function cmd_upload() {
         --allow_quoted_newlines \
         --sync \
         --autodetect \
-        "metriclite.TRANSIENT_GIT_$metriclite_gitname" "target/git/$metriclite_gitname.csv.gz"
+        "metriclite.TRANSIENT_GIT_${metriclite_gitname}_COMMITS" "target/git/commits-$metriclite_gitname.csv.gz"
+
+    bq load --source_format CSV \
+        --skip_leading_rows 1 \
+        --replace \
+        --allow_quoted_newlines \
+        --sync \
+        --autodetect \
+        "metriclite.TRANSIENT_GIT_${metriclite_gitname}_FILES" "target/git/files-$metriclite_gitname.csv.gz"
 }
 
 function cmd_update() {
@@ -39,6 +69,7 @@ function cmd_update() {
     local metriclite_giturl="${2?'metriclite_giturl'}"
     cmd_download "$metriclite_gitname" "$metriclite_giturl"
     cmd_convert_commits "$metriclite_gitname"
+    cmd_convert_files "$metriclite_gitname"
     cmd_upload "$metriclite_gitname"
 }
 
